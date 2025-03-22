4a88ab2b9e29ebfc0eeb5c81da5005f8877e3682
QuocThanh <hongloi123123@gmail.com>
2025-03-22 22:00:54 +0700

    code template

---
+++ lib/utils/quiz_images.dart
@@ -0,0 +1,34 @@
+const url =
+    "https://github.com/muhammad-fiaz/QuizApp-Flutter/assets/75434191/06e28a7a-6239-4a3f-af88-4a49b272ce9e";
+
+const quiziccommunication = url;
+const quiziccommunication2 = url;
+const quiziccourse1 = url;
+const quiziccourse2 = url;
+const quiziccourse3 = url;
+const quizicinfo = url;
+const quizicjava = url;
+const quizicmarketing = url;
+const quizicnotification = url;
+const quizicstudy1 = url;
+const quizicstudy2 = url;
+const quizicquiz1 = "images/quizz/quiz_ic_quiz1.png";
+const quizicquiz2 = "images/quizz/quiz_ic_quiz2.png";
+const quizicquiz = "images/quizz/quiz_ic_quiz.svg";
+const quizichomes = "images/quizz/quiz_ic_home.svg";
+const quizicuser = "images/quizz/quiz_ic_user.svg";
+const quizicfacebook = "images/quizz/quiz_ic_facebook.svg";
+const quizicgoogle = "images/quizz/quiz_ic_google.svg";
+const quizicmail = "images/quizz/quiz_ic_mail.svg";
+const quizictwitter = "images/quizz/quiz_ic_twitter.svg";
+
+const quizicGrid = "images/quizz/quiz_ic_grid.png";
+const quizicList = "images/quizz/quiz_ic_list.png";
+const quizimgPeople1 = url;
+const quizimgPeople2 = url;
+
+const quiziclist1 = "images/quizz/quiz_ic_list1.png";
+const quiziclist2 = "images/quizz/quiz_ic_list2.png";
+const quiziclist3 = "images/quizz/quiz_ic_list3.png";
+const quiziclist4 = "images/quizz/quiz_ic_list4.png";
+const quiziclist5 = "images/quizz/quiz_ic_list5.png";
